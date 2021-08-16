b0cf2e67c93f8fbebc5fd1c531210bdeff8a3637
jhlland <jhlland@naver.com>
2021-08-16 15:33:24 +0900

    first project

---
+++ app/src/main/java/com/example/firstapplication/Bottom_nav_menu.java
@@ -0,0 +1,19 @@
+package com.example.firstapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
+public class Bottom_nav_menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bottomviewnavigation);
+
+    }
+
+}
