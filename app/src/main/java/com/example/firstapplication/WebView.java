8ed7a555f063b1b41b225dca53adae69f6a055bf
jhlland01 <jhlland01@naver.com>
2021-08-20 19:22:44 +0900

    second project

---
+++ app/src/main/java/com/example/firstapplication/WebView.java
@@ -0,0 +1,6 @@
+package com.example.firstapplication;
+
+import android.app.Activity;
+
+public class WebView extends Activity {
+}
---
+++ app/src/main/java/com/example/firstapplication/WebView.kt
@@ -0,0 +1,15 @@
+package com.example.firstapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebViewClient
+
+
+
+class WebView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_view)
+
+    }
+}
